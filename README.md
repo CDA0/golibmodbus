97386de9ef9f1c841dec0866e5f236ed31e3c8cb
Craig Anderson <craig.anderson0@gmail.com>
2015-03-26 10:51:51 +0000

    added 'object' accessing

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # golibmodbus
+This is a binding to [libmodbus](https://github.com/stephane/libmodbus) (http://libmodbus.org)
+
+cgo flags are set for OSX with libmodbus installed from homebrew.
+
+#Examples
+##Create TCP client
+```go
+mb, err := golibmodbus.NewTCP("127.0.0.1", 1502)
+
+mb.SetDebug(1)
+mb.Connect()
+mb.SetSlave(1)
+mb.SetErrorRecovery(2)
+
+reg, err := mb.ReadRegisters(1, 26)
+fmt.Println(reg)
+
+mb.Close()
+mb.Free()
+```
--- golibmodbus.go
+++ golibmodbus.go
@@ -1,4 +1,4 @@
-package libmodbus
+package golibmodbus
 
 /*
 #cgo CFLAGS: -I/usr/local/include/modbus
@@ -21,14 +21,22 @@ type uintt_t C.uint8_t
 type uint16_t C.uint16_t
 type uint32_t C.uint32_t
 
+type Modbus struct {
+  ctx *C.modbus_t
+  mb_mapping *C.modbus_mapping_t
+}
+
+func (m *Modbus) VersionString() string { return VersionString() }
 func VersionString() string {
   return C.LIBMODBUS_VERSION_STRING
 }
 
+func (m *Modbus) VersionHex() int { return VersionHex() }
 func VersionHex() int {
   return C.LIBMODBUS_VERSION_HEX
 }
 
+func (m *Modbus) VersionCheck(major uint, minor uint, micro uint) bool { return VersionCheck(major, minor, micro) }
 func VersionCheck(major uint, minor uint, micro uint) bool {
   if major == VersionMajor() && minor == VersionMinor() && micro == VersionMicro() {
     return true
@@ -36,19 +44,22 @@ func VersionCheck(major uint, minor uint, micro uint) bool {
   return false
 }
 
+func (m *Modbus) VersionMajor() uint { return VersionMajor() }
 func VersionMajor() uint {
   return uint(C.libmodbus_version_major)
 }
 
+func (m *Modbus) VersionMinor() uint { return VersionMinor() }
 func VersionMinor() uint {
   return uint(C.libmodbus_version_minor)
 }
 
+func (m *Modbus) VersionMicro() uint { return VersionMicro() }
 func VersionMicro() uint {
   return uint(C.libmodbus_version_micro)
 }
 
-func NewRTU(device string, baud int, parity byte, dataBit int, stopBit int) (*C.modbus_t, error) {
+func NewRTU(device string, baud int, parity byte, dataBit int, stopBit int) (*Modbus, error) {
   cDevice := C.CString(device)
   cBaud := C.int(baud)
   cParity := C.char(parity)
@@ -57,170 +68,183 @@ func NewRTU(device string, baud int, parity byte, dataBit int, stopBit int) (*C.
 
   ctx, err := C.modbus_new_rtu(cDevice, cBaud, cParity, cDataBit, cStopBit)
 
+  m := new(Modbus)
+
   err = CheckError(err)
 
-  return ctx, err
+  if err == nil { m.ctx = ctx }
+
+  return m, err
 }
 
-func NewTCP(ip string, port int) (*C.modbus_t, error) {
+func NewTCP(ip string, port int) (*Modbus, error) {
   cIP := C.CString(ip)
   cPort := C.int(port)
 
   ctx, err :=  C.modbus_new_tcp(cIP, cPort)
 
+  m := new(Modbus)
+
   err = CheckError(err)
 
-  return ctx, err
+  if err == nil { m.ctx = ctx }
+
+  return m, err
 }
 
-func NewTCPPI(node string, service string) (*C.modbus_t, error) {
+func NewTCPPI(node string, service string) (*Modbus, error) {
   cNode := C.CString(node)
   cService := C.CString(service)
 
   ctx, err :=  C.modbus_new_tcp_pi(cNode, cService)
 
+  m := new(Modbus)
+
   err = CheckError(err)
 
-  return ctx, err
+  if err == nil { m.ctx = ctx }
+
+  return m, err
 }
 
-func RTUGetSerialMode(ctx *C.modbus_t) (int, error) {
-  r, err :=  C.modbus_rtu_get_serial_mode(ctx)
+func (m *Modbus) RTUGetSerialMode() (int, error) {
+  r, err :=  C.modbus_rtu_get_serial_mode(m.ctx)
 
   err = CheckError(err)
 
   return int(r), err
 }
 
-func RTUSetSerialMode(ctx *C.modbus_t, mode int) (int, error) {
+func (m *Modbus) RTUSetSerialMode(mode int) (int, error) {
   cMode := C.int(mode)
 
-  r, err :=  C.modbus_rtu_set_serial_mode(ctx, cMode)
+  r, err :=  C.modbus_rtu_set_serial_mode(m.ctx, cMode)
 
   err = CheckError(err)
 
   return int(r), err
 }
 
-func RTUGetRTS(ctx *C.modbus_t) (int, error) {
-  r, err :=  C.modbus_rtu_get_rts(ctx)
+func (m *Modbus) RTUGetRTS() (int, error) {
+  r, err :=  C.modbus_rtu_get_rts(m.ctx)
 
   err = CheckError(err)
 
   return int(r), err
 }
 
-func RTUSetRTS(ctx *C.modbus_t, mode int) (int, error) {
+func (m *Modbus) RTUSetRTS(mode int) (int, error) {
   cMode := C.int(mode)
-  r, err :=  C.modbus_rtu_set_rts(ctx, cMode)
+  r, err :=  C.modbus_rtu_set_rts(m.ctx, cMode)
 
   err = CheckError(err)
 
   return int(r), err
 }
 
-func Free(ctx *C.modbus_t) {
-  C.modbus_free(ctx)
+func (m *Modbus) Free() {
+  C.modbus_free(m.ctx)
 }
 
-func SetSlave(ctx *C.modbus_t, slave int) (int, error) {
+func (m *Modbus) SetSlave(slave int) (int, error) {
   cSlave := C.int(slave)
 
-  r, err :=  C.modbus_set_slave(ctx, cSlave)
+  r, err :=  C.modbus_set_slave(m.ctx, cSlave)
 
   err = CheckError(err)
 
   return int(r), err
 }
 
-func SetDebug(ctx *C.modbus_t, flag int) (int, error) {
+func (m *Modbus) SetDebug(flag int) (int, error) {
   cFlag := C.int(flag)
 
-  r, err :=  C.modbus_set_debug(ctx, cFlag)
+  r, err :=  C.modbus_set_debug(m.ctx, cFlag)
 
   err = CheckError(err)
 
   return int(r), err
 }
 
-func GetByteTimeout(ctx *C.modbus_t, toSec uint32, toUsec uint32) (int, error) {
+func (m *Modbus) GetByteTimeout(toSec uint32, toUsec uint32) (int, error) {
   cToSec := C.uint32_t(toSec)
   cToUsec := C.uint32_t(toUsec)
 
-  r, err :=  C.modbus_get_byte_timeout(ctx, &cToSec, &cToUsec)
+  r, err :=  C.modbus_get_byte_timeout(m.ctx, &cToSec, &cToUsec)
 
   err = CheckError(err)
 
   return int(r), err
 }
 
-func SetByteTimeout(ctx *C.modbus_t, toSec uint32, toUsec uint32) (int, error) {
+func (m *Modbus) SetByteTimeout(toSec uint32, toUsec uint32) (int, error) {
   cToSec := C.uint32_t(toSec)
   cToUsec := C.uint32_t(toUsec)
 
-  r, err :=  C.modbus_set_byte_timeout(ctx, cToSec, cToUsec)
+  r, err :=  C.modbus_set_byte_timeout(m.ctx, cToSec, cToUsec)
 
   err = CheckError(err)
 
   return int(r), err
 }
 
-func GetResponseTimeout(ctx *C.modbus_t, toSec uint32, toUsec uint32) (int, error) {
+func (m *Modbus) GetResponseTimeout(toSec uint32, toUsec uint32) (int, error) {
   cToSec := C.uint32_t(toSec)
   cToUsec := C.uint32_t(toUsec)
 
-  r, err :=  C.modbus_get_response_timeout(ctx, &cToSec, &cToUsec)
+  r, err :=  C.modbus_get_response_timeout(m.ctx, &cToSec, &cToUsec)
 
   err = CheckError(err)
 
   return int(r), err
 }
 
-func SetResponseTimeout(ctx *C.modbus_t, toSec uint32, toUsec uint32) (int, error) {
+func (m *Modbus) SetResponseTimeout(toSec uint32, toUsec uint32) (int, error) {
   cToSec := C.uint32_t(toSec)
   cToUsec := C.uint32_t(toUsec)
 
-  r, err :=  C.modbus_set_response_timeout(ctx, cToSec, cToUsec)
+  r, err :=  C.modbus_set_response_timeout(m.ctx, cToSec, cToUsec)
 
   err = CheckError(err)
 
   return int(r), err
 }
 
-func SetErrorRecovery(ctx *C.modbus_t, errorRecovery uint) (int, error) {
-  r, err :=  C.modbus_set_error_recovery(ctx, C.modbus_error_recovery_mode(errorRecovery))
+func (m *Modbus) SetErrorRecovery(errorRecovery uint) (int, error) {
+  r, err :=  C.modbus_set_error_recovery(m.ctx, C.modbus_error_recovery_mode(errorRecovery))
 
   err = CheckError(err)
 
   return int(r), err
 }
 
-func SetSocket(ctx *C.modbus_t, s int) (int, error) {
+func (m *Modbus) SetSocket(s int) (int, error) {
   cS := C.int(s)
 
-  r, err :=  C.modbus_set_socket(ctx, cS)
+  r, err :=  C.modbus_set_socket(m.ctx, cS)
 
   err = CheckError(err)
 
   return int(r), err
 }
 
-func GetSocket(ctx *C.modbus_t) (int, error) {
-  r, err :=  C.modbus_get_socket(ctx)
+func (m *Modbus) GetSocket() (int, error) {
+  r, err :=  C.modbus_get_socket(m.ctx)
 
   err = CheckError(err)
 
   return int(r), err
 }
 
-func GetHeaderLength(ctx *C.modbus_t) (int, error) {
-  r, err :=  C.modbus_get_header_length(ctx)
+func (m *Modbus) GetHeaderLength() (int, error) {
+  r, err :=  C.modbus_get_header_length(m.ctx)
 
   err = CheckError(err)
 
   return int(r), err
 }
 
+func (m *Modbus) SetBitsFromByte(index int, value uint8) []uint8 { return SetBitsFromByte(index, value) }
 func SetBitsFromByte(index int, value uint8) []uint8 {
   cIndex := C.int(index)
   cValue := C.uint8_t(value)
@@ -233,6 +257,7 @@ func SetBitsFromByte(index int, value uint8) []uint8 {
   return output
 }
 
+func (m *Modbus) SetBitsFromBytes(index int, nbBits uint, tabByte *uint8) []uint8 { return SetBitsFromBytes(index, nbBits, tabByte) }
 func SetBitsFromBytes(index int, nbBits uint, tabByte *uint8) []uint8 {
   cIndex := C.int(index)
   cNbBits := C.uint(nbBits)
@@ -246,6 +271,7 @@ func SetBitsFromBytes(index int, nbBits uint, tabByte *uint8) []uint8 {
   return output
 }
 
+func (m *Modbus) GetByteFromBits(source *uint8, index int, nbBits uint) byte { return GetByteFromBits(source, index, nbBits) }
 func GetByteFromBits(source *uint8, index int, nbBits uint) byte {
   cSource := (*C.uint8_t)(unsafe.Pointer(source))
   cIndex := C.int(index)
@@ -254,12 +280,14 @@ func GetByteFromBits(source *uint8, index int, nbBits uint) byte {
   return byte(C.modbus_get_byte_from_bits(cSource, cIndex,cNbBits))
 }
 
+func (m *Modbus) GetFloat(src *uint16) float32 { return GetFloat(src) }
 func GetFloat(src *uint16) float32 {
   cSrc := (*C.uint16_t)(unsafe.Pointer(src))
 
   return float32(C.modbus_get_float(cSrc))
 }
 
+func (m *Modbus) SetFloat(f float32) []uint16 { return SetFloat(f) }
 func SetFloat(f float32) []uint16 {
   cF := C.float(f)
 
@@ -271,12 +299,14 @@ func SetFloat(f float32) []uint16 {
   return output
 }
 
+func (m *Modbus) GetFloatDcba(src *uint16) float32 { return GetFloatDcba(src) }
 func GetFloatDcba(src *uint16) float32 {
   cSrc := (*C.uint16_t)(unsafe.Pointer(src))
 
   return float32(C.modbus_get_float_dcba(cSrc))
 }
 
+func (m *Modbus) SetFloatDcba(f float32) []uint16 { return SetFloatDcba(f) }
 func SetFloatDcba(f float32) []uint16 {
   cF := C.float(f)
 
@@ -288,138 +318,138 @@ func SetFloatDcba(f float32) []uint16 {
   return output
 }
 
-func Connect(ctx *C.modbus_t) (int, error) {
-  r, err :=  C.modbus_connect(ctx)
+func (m *Modbus) Connect() (int, error) {
+  r, err :=  C.modbus_connect(m.ctx)
 
   err = CheckError(err)
 
   return int(r), err
 }
 
-func Close(ctx *C.modbus_t) {
-  C.modbus_close(ctx)
+func (m *Modbus) Close() {
+  C.modbus_close(m.ctx)
 }
 
-func Flush(ctx *C.modbus_t) (int, error) {
-  r, err := C.modbus_flush(ctx)
+func (m *Modbus) Flush() (int, error) {
+  r, err := C.modbus_flush(m.ctx)
 
   err = CheckError(err)
 
   return int(r), err
 }
 
-func ReadBits(ctx *C.modbus_t, addr int, nb int) ([]uint8, error) {
+func (m *Modbus) ReadBits(addr int, nb int) ([]uint8, error) {
   cAddr := C.int(addr)
   cNb := C.int(nb)
 
   output := make([]uint8, nb)
   cOutput := (*C.uint8_t)(&output[0])
 
-  _, err :=  C.modbus_read_bits(ctx, cAddr, cNb, cOutput)
+  _, err :=  C.modbus_read_bits(m.ctx, cAddr, cNb, cOutput)
 
   err = CheckError(err)
 
   return output, err
 }
 
-func ReadInputBits(ctx *C.modbus_t, addr int, nb int) ([]uint8, error) {
+func (m *Modbus) ReadInputBits(addr int, nb int) ([]uint8, error) {
   cAddr := C.int(addr)
   cNb := C.int(nb)
 
   output := make([]uint8, nb)
   cOutput := (*C.uint8_t)(&output[0])
 
-  _, err := C.modbus_read_input_bits(ctx, cAddr, cNb, cOutput)
+  _, err := C.modbus_read_input_bits(m.ctx, cAddr, cNb, cOutput)
 
   err = CheckError(err)
 
   return output, err
 }
 
-func ReadRegisters(ctx *C.modbus_t, addr int, nb int) ([]uint16, error) {
+func (m *Modbus) ReadRegisters(addr int, nb int) ([]uint16, error) {
   cAddr := C.int(addr)
   cNb := C.int(nb)
 
   output := make([]uint16, nb)
   cOutput := (*C.uint16_t)(&output[0])
 
-  _, err := C.modbus_read_registers(ctx, cAddr, cNb, cOutput)
+  _, err := C.modbus_read_registers(m.ctx, cAddr, cNb, cOutput)
 
   err = CheckError(err)
 
   return output, err
 }
 
-func ReadInputRegisters(ctx *C.modbus_t, addr int, nb int, dest *uint16) ([]uint16, error) {
+func (m *Modbus) ReadInputRegisters(addr int, nb int, dest *uint16) ([]uint16, error) {
   cAddr := C.int(addr)
   cNb := C.int(nb)
 
   output := make([]uint16, nb)
   cOutput := (*C.uint16_t)(&output[0])
 
-  _, err := C.modbus_read_input_registers(ctx, cAddr, cNb, cOutput)
+  _, err := C.modbus_read_input_registers(m.ctx, cAddr, cNb, cOutput)
 
   err = CheckError(err)
 
   return output, err
 }
 
-func ReportSlaveId(ctx *C.modbus_t, maxDest int, dest *uint8) ([]uint8, error) {
+func (m *Modbus) ReportSlaveId(maxDest int, dest *uint8) ([]uint8, error) {
   cMaxDest := C.int(maxDest)
 
   output := make([]uint8, maxDest)
   cOutput := (*C.uint8_t)(&output[0])
 
-  _, err := C.modbus_report_slave_id(ctx, cMaxDest, cOutput)
+  _, err := C.modbus_report_slave_id(m.ctx, cMaxDest, cOutput)
 
   err = CheckError(err)
 
   return output, err
 }
 
-func WriteBit(ctx *C.modbus_t, addr int, status int) (int, error) {
+func (m *Modbus) WriteBit(addr int, status int) (int, error) {
   cAddr := C.int(addr)
   cStatus := C.int(status)
 
-  r, err :=  C.modbus_write_bit(ctx, cAddr, cStatus)
+  r, err :=  C.modbus_write_bit(m.ctx, cAddr, cStatus)
 
   err = CheckError(err)
 
   return int(r), err
 }
 
-func WriteRegister(ctx *C.modbus_t, addr int, value int) (int, error) {
+func (m *Modbus) WriteRegister(addr int, value int) (int, error) {
   cAddr := C.int(addr)
   cValue := C.int(value)
 
-  r, err :=  C.modbus_write_register(ctx, cAddr, cValue)
+  r, err :=  C.modbus_write_register(m.ctx, cAddr, cValue)
 
   err = CheckError(err)
 
   return int(r), err
 }
 
-func WriteBits(ctx *C.modbus_t, addr int, nb int, src *uint8) (int, error) {
+func (m *Modbus) WriteBits(addr int, nb int, src *uint8) (int, error) {
   cAddr := C.int(addr)
   cNb := C.int(nb)
   cSrc := (*C.uint8_t)(unsafe.Pointer(src))
 
-  r, err :=  C.modbus_write_bits(ctx, cAddr, cNb, cSrc)
+  r, err :=  C.modbus_write_bits(m.ctx, cAddr, cNb, cSrc)
 
   return int(r), err
 }
 
-func WriteRegisters(ctx *C.modbus_t, addr int, nb int, src *uint16) (int, error) {
+func (m *Modbus) WriteRegisters(addr int, nb int, src *uint16) (int, error) {
   cAddr := C.int(addr)
   cNb := C.int(nb)
   cSrc := (*C.uint16_t)(unsafe.Pointer(src))
 
-  r, err :=  C.modbus_write_registers(ctx, cAddr, cNb, cSrc)
+  r, err :=  C.modbus_write_registers(m.ctx, cAddr, cNb, cSrc)
 
   return int(r), err
 }
 
-func WriteAndReadRegisters(ctx *C.modbus_t, writeAddr int, writeNb int, src *uint16, readAddr int, readNb int) ([]uint16, error) {
+func (m *Modbus) WriteAndReadRegisters(writeAddr int, writeNb int, src *uint16, readAddr int, readNb int) ([]uint16, error) {
   cWriteAddr := C.int(writeAddr)
   cWriteNb := C.int(writeNb)
   cSrc := (*C.uint16_t)(unsafe.Pointer(src))
@@ -429,46 +459,46 @@ func WriteAndReadRegisters(ctx *C.modbus_t, writeAddr int, writeNb int, src *uin
   output := make([]uint16, readNb)
   cOutput := (*C.uint16_t)(&output[0])
 
-  _, err := C.modbus_write_and_read_registers(ctx, cWriteAddr, cWriteNb, cSrc, cReadAddr, cReadNb, cOutput)
+  _, err := C.modbus_write_and_read_registers(m.ctx, cWriteAddr, cWriteNb, cSrc, cReadAddr, cReadNb, cOutput)
 
   err = CheckError(err)
 
   return output, err
 }
 
-func SendRawRequest(ctx *C.modbus_t, rawReq *uint8, rawReqLen int) (int, error) {
+func (m *Modbus) SendRawRequest(rawReq *uint8, rawReqLen int) (int, error) {
   cRawReq := (*C.uint8_t)(unsafe.Pointer(rawReq))
   cRawReqLen := C.int(rawReqLen)
 
-  r, err :=  C.modbus_send_raw_request(ctx, cRawReq, cRawReqLen)
+  r, err :=  C.modbus_send_raw_request(m.ctx, cRawReq, cRawReqLen)
 
   err = CheckError(err)
 
   return int(r), err
 }
 
-func ReceiveConfirmation(ctx *C.modbus_t, rsp *uint8) (int, error) {
+func (m *Modbus) ReceiveConfirmation(rsp *uint8) (int, error) {
   cRsp := (*C.uint8_t)(unsafe.Pointer(rsp))
 
-  r, err :=  C.modbus_receive_confirmation(ctx, cRsp)
+  r, err :=  C.modbus_receive_confirmation(m.ctx, cRsp)
 
   err = CheckError(err)
 
   return int(r), err
 }
 
-func ReplyException(ctx *C.modbus_t, req *uint8, exceptionCode int) (int, error) {
+func (m *Modbus) ReplyException(req *uint8, exceptionCode int) (int, error) {
   cReq := (*C.uint8_t)(unsafe.Pointer(req))
   cExceptionCode := C.uint(exceptionCode)
 
-  r, err :=  C.modbus_reply_exception(ctx, cReq, cExceptionCode)
+  r, err :=  C.modbus_reply_exception(m.ctx, cReq, cExceptionCode)
 
   err = CheckError(err)
 
   return int(r), err
 }
 
-func MappingNew(nbBits int, nbInputBits int, nbRegisters int, nbInputRegisters int) (*C.modbus_mapping_t, error) {
+func (m *Modbus) MappingNew(nbBits int, nbInputBits int, nbRegisters int, nbInputRegisters int) error {
   cNbBits := C.int(nbBits)
   cNbInputBits := C.int(nbInputBits)
   cNbRegisters := C.int(nbRegisters)
@@ -478,26 +508,28 @@ func MappingNew(nbBits int, nbInputBits int, nbRegisters int, nbInputRegisters i
 
   err = CheckError(err)
 
-  return mb_mapping, err
+  if err == nil { m.mb_mapping = mb_mapping }
+
+  return err
 }
 
-func MappingFree(mbMapping *C.modbus_mapping_t) {
-  C.modbus_mapping_free(mbMapping)
+func (m *Modbus) MappingFree() {
+  C.modbus_mapping_free(m.mb_mapping)
 }
 
-func Receive(ctx *C.modbus_t, req *uint8) (int, error) {
-  r, err :=  C.modbus_receive(ctx, (*C.uint8_t)(unsafe.Pointer(req)))
+func (m *Modbus) Receive(req *uint8) (int, error) {
+  r, err :=  C.modbus_receive(m.ctx, (*C.uint8_t)(unsafe.Pointer(req)))
 
   err = CheckError(err)
 
   return int(r), err
 }
 
-func Reply(ctx *C.modbus_t, req *uint8, reqLength int, mb_mapping *C.modbus_mapping_t) (int, error) {
+func (m *Modbus) Reply(req *uint8, reqLength int) (int, error) {
   cReq := (*C.uint8_t)(unsafe.Pointer(req))
   cReqLength := C.int(reqLength)
 
-  r, err :=  C.modbus_reply(ctx,  cReq, cReqLength, mb_mapping)
+  r, err :=  C.modbus_reply(m.ctx,  cReq, cReqLength, m.mb_mapping)
 
   err = CheckError(err)
 
